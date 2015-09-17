d58b2818b12d9941253f0cc57b2c39257d864641
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2015-09-17 22:49:50 +0100

    read ports json config

--- load_balancer.go
+++ load_balancer.go
@@ -1,29 +1,50 @@
 package main
 
 import (
-		"log"
-		"net/http"
-		"net/http/httputil"
-		"math/rand"
-		"strconv"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"math/rand"
+	"strconv"
+
+	"encoding/json"
+	"os"
+	"fmt"
 )
 
-func main() {
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+type Configuration struct {
+	PortsFirst  int
+	PortsNum    int
+}
 
-				ports := []int{5555, 5556, 5557, 5558, 5559, 5560}
-				for i := range ports {
-						j := rand.Intn(i + 1)
-						ports[i], ports[j] = ports[j], ports[i]
-				}
-				port := ports[0]
-				director := func(req *http.Request) {
-						req = r
-						req.URL.Scheme = "http"
-						req.URL.Host = "localhost:"+strconv.Itoa(port)
-				}
-				proxy := &httputil.ReverseProxy{Director: director}
-				proxy.ServeHTTP(w, r)
-		})
-		log.Fatal(http.ListenAndServe(":8181", nil))
+
+func main() {
+
+	file, _ := os.Open("./config/thin.json")
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil { fmt.Println("error:", err) }
+	fmt.Println(configuration.PortsFirst)
+	fmt.Println(configuration.PortsNum)
+
+
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		ports := []int{5555, 5556, 5557, 5558, 5559, 5560}
+		for i := range ports {
+				j := rand.Intn(i + 1)
+				ports[i], ports[j] = ports[j], ports[i]
+		}
+		port := ports[0]
+		director := func(req *http.Request) {
+				req = r
+				req.URL.Scheme = "http"
+				req.URL.Host = "localhost:"+strconv.Itoa(port)
+		}
+		proxy := &httputil.ReverseProxy{Director: director}
+		go proxy.ServeHTTP(w, r)
+	})
+	log.Fatal(http.ListenAndServe(":8181", nil))
 }
